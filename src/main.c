2174a031aeaaa680a16f841648e5f245fa6a468a
toluschr <toluschr@protonmail.com>
2019-12-23 12:55:19 +0100

    Add: Code should work on Windows now, but is completely unreadable

--- src/main.c
+++ src/main.c
@@ -6,15 +6,23 @@
 #include <stdio.h>
 
 #if defined(_WIN32)
-  #include <windows.h>
+#include <sys/stat.h>
+#include <windows.h>
+#include <process.h>
+#include <tlhelp32.h>
+#include <winbase.h>
+
+#define PATH_DELIMETER '\\'
 #endif
 
 #if defined(UNIX) || defined(__unix__)
-  #include <sys/reboot.h>
-  #include <unistd.h>
+#include <sys/reboot.h>
+#include <unistd.h>
+
+#define PATH_DELIMETER '/'
 #endif
 
-#define TIMEOUT 1000 * 1000 * 10
+#define TIMEOUT 1000 * 10
 
 void quit (const char *message, int code) {
   printf("%s\n", message);
@@ -28,6 +36,17 @@ bool sisdisit (const char *string, size_t l_string) {
   return 1;
 }
 
+
+int __getpid (void) {
+#if defined(UNIX) || defined(__unix__)
+  return getpid();
+#endif
+#if defined(_WIN32)
+  return GetCurrentProcessId();
+#endif
+}
+
+#if defined(LINUX) || defined(__linux__)
 void killall (const char *name, int sig) {
   DIR *proc = opendir("/proc");
 
@@ -35,6 +54,7 @@ void killall (const char *name, int sig) {
   while ((entry = readdir(proc)) != NULL) {
     if (strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".") == 0) continue;
     if (!sisdisit(entry->d_name, strlen(entry->d_name))) continue;
+  
     if (atoi(entry->d_name) == getpid()) continue;
 
     char status_path[4096] = {0};
@@ -52,6 +72,26 @@ void killall (const char *name, int sig) {
 
   closedir(proc);
 }
+#endif
+#if defined(_WIN32)
+void killall (const char *name, int sig) {
+  HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+  PROCESSENTRY32 pEntry;
+  pEntry.dwSize = sizeof (pEntry);
+  BOOL hRes = Process32First(hSnapShot, &pEntry);
+  do {
+    if (strstr(pEntry.szExeFile, name) == NULL) continue;
+    if (pEntry.th32ProcessID == __getpid()) continue;
+
+    HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0, (DWORD)pEntry.th32ProcessID);
+    if (hProcess == NULL)  continue;
+    
+    TerminateProcess(hProcess, sig);
+    CloseHandle(hProcess);
+  } while ((hRes = Process32Next(hSnapShot, &pEntry)));
+  CloseHandle(hSnapShot);
+}
+#endif
 
 int poweroff () {
 #if defined(UNIX) || defined(__unix__)
@@ -59,16 +99,30 @@ int poweroff () {
 #endif
 
 #if defined(_WIN32)
-  return ExitWindowsEx(RB_POWER_OFF);
+  return ExitWindowsEx(EWX_POWEROFF, SHTDN_REASON_FLAG_PLANNED);
+#endif
+}
+
+int msleep (int millis) {
+#if defined(UNIX) || defined(__unix__)
+  return usleep(millis * 1000);
+#endif
+
+#if defined(_WIN32)
+  Sleep(millis);
+  return 0;
 #endif
 }
 
 int main (int argc, char **argv) {
   if (argc != 2) quit("!> Missing Library Path", 1);
+#if defined(UNIX) || defined(__unix__)
   if (geteuid() != 0) quit("!> Requires Root Permissions", 1);
+#endif
 
   char downloading_path[4096] = {0};
-  sprintf(downloading_path, "%s/downloading", argv[1]);
+
+  sprintf(downloading_path, "%s%cdownloading", argv[1], PATH_DELIMETER);
 
   DIR *downloading = opendir(downloading_path);
   int queued;
@@ -78,17 +132,29 @@ int main (int argc, char **argv) {
     struct dirent *entry;
     while ((entry = readdir(downloading)) != NULL) {
       if (strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".") == 0) continue;
+#if defined(_WIN32)
+      struct _stat st;
+      char buffer[4096] = {0};
+      sprintf(buffer, "%s%c%s", downloading_path, PATH_DELIMETER, entry->d_name);
+      _stat(buffer, &st);
+      queued += (st.st_mode & S_IFDIR != 0);
+#else
       queued += (entry->d_type == 4);
+#endif
     }
     rewinddir(downloading);
     printf(":> Queued %i Download(s)\n", queued);
-  } while (queued > 0 && usleep(TIMEOUT) == 0);
+  } while (queued > 0 && msleep(TIMEOUT) == 0);
 
   closedir(downloading);
 
   printf(":> Killing Steam!\n");
+#if defined(_WIN32)
+  killall("steam", SIGTERM);
+#else
   killall("steam", SIGQUIT);
-  usleep(TIMEOUT);
+#endif
+  msleep(TIMEOUT);
   poweroff();
 }
 
